18dd5cbf61bdd163b4ab27e8c32944f71e812ab0
zshaan25 <154325262+zshaan25@users.noreply.github.com>
2025-05-08 22:36:56 +0500

    Update ProductCard.jsx

--- src/components/views/home/ProductCard.jsx
+++ src/components/views/home/ProductCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ProductCard = ({ name, price, image }) => {
   return (
     <div className="border rounded-lg overflow-hidden shadow hover:shadow-md transition">
-      <img src={image} alt={name} className="w-full h-48 object-cover" />
+      <img src={image} alt={name} className="w-full h-30 object-cover" />
       <div className="p-4">
         <h3 className="text-lg font-semibold text-gray-800">{name}</h3>
         <p className="text-indigo-600 font-bold">${price}</p>
